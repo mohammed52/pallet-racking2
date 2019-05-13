bc32cc8b108a4c3ca6cd7a18f8e3b89ef6bca239
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2019-05-13 11:48:14 +0500

    wip: check heroku and github

--- config/app.js
+++ config/app.js
@@ -1,11 +1,12 @@
-import { ENV } from './env';
+import { ENV } from "./env";
 
-export const isProduction = ENV === 'production';
-export const isDebug = ENV === 'development';
-export const isClient = typeof window !== 'undefined';
+export const isProduction = ENV === "production";
+export const isDebug = ENV === "development";
+export const isClient = typeof window !== "undefined";
 
-export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'https://ymtm-docs.herokuapp.com/';
+export const apiEndpoint = isDebug
+	? "http://localhost:5000"
+	: "https://pallet-racking2.herokuapp.com/";
 // export const apiEndpoint = isDebug ? 'http://localhost:5000' : 'http://localhost:5000';
 // Replace with 'UA-########-#' or similar to enable tracking
 export const trackingID = null;
-
